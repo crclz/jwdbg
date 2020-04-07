a4af514ab78661a390fb16775c56526f6ac6eeb6
crclz <crucialize@foxmail.com>
2020-04-07 22:14:49 +0800

    refactor

--- jwdbg.py
+++ jwdbg.py
@@ -9,114 +9,94 @@ with open(case_file, 'r', encoding="utf-8") as f:
     testcase = yaml.load(f, Loader=yaml.SafeLoader)
 
 
-def get_input_output(data, until):
-    input_str = ""
-    output_str = ""
+# get input and output lines from test data
+# make sure each item of these 2 lists does not end with \n, \r\n (by calling strip())
+input_lines = []
+output_lines = []
 
-    i = 0
-    for item in data:
-        if not i < until:
-            break
+# 1 command may has more than 1 line of output
+# belong_to[output_line_index]==its input line index
+belong_to = dict()
 
-        output_line = None
+for item in testcase['data']:
 
-        if isinstance(item, str):
-            # input with no output
-            line = str(item)
-        elif isinstance(item, list):
-            # input with output
-            if len(item) != 2:
-                raise Exception(f"Test data wrong format. {str(item)}")
-            line = str(item[0])
-            output_line = str(item[1])
-        else:
+    current_output = None
+
+    if isinstance(item, str):
+        # input with no output
+        current_input_line = str(item).strip()
+    elif isinstance(item, list):
+        # input with output
+        if len(item) != 2:
             raise Exception(f"Test data wrong format. {str(item)}")
+        current_input_line = str(item[0]).strip()
+        current_output = str(item[1]).strip()
+    else:
+        raise Exception(f"Test data wrong format. {str(item)}")
 
-        if not line.endswith("\n"):
-            line += "\n"
-        input_str += line
+    input_lines.append(current_input_line)
 
-        if output_line != None:
-            if not output_line.endswith("\n"):
-                output_line += "\n"
-            output_str += output_line
-
-        i += 1
-
-    return input_str, output_str
-
-
-def get_program_output(cmd, input_str):
-    input_data = input_str.encode()
-    output = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                              stdin=subprocess.PIPE).communicate(input_data)[0]
-
-    return output.decode()
+    if current_output != None:
+        for line in current_output.splitlines(keepends=False):
+            line = line.strip()
+            output_lines.append(line)
+            belong_to[len(output_lines)-1] = len(input_lines)-1
 
 
-input_str, expected_output = get_input_output(testcase['data'], 99999)
+# run program and get output
 
 os.chdir("E:/personal/学习/大二下/面向对象/jw_idea/target/classes")
+cmd = "java Test"
+input_data = "\n".join(input_lines).encode("utf-8")
+output_data = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                               stdin=subprocess.PIPE).communicate(input_data)[0]
+actual_lines = output_data.decode().splitlines(keepends=False)
 
-actual_output = get_program_output("java Test", input_str)
+# compare actual output lines and expected lines
 
-actual_lines = actual_output.splitlines(keepends=False)
-expected_lines = expected_output.splitlines(keepends=False)
-
-smaller_len = min(len(actual_lines), len(expected_lines))
-
-# compare actual lines and expected lines
-
+smaller_len = min(len(actual_lines), len(output_lines))
 ok = True
-cursor = 0
-tc_cursor = 0
-for item in testcase['data']:
-    if not cursor < smaller_len:
+
+i = 0
+while i < smaller_len:
+    if output_lines[i] != actual_lines[i]:
+        ok = False
+        print("test failure")
+        print("================")
+        print(f"expected:\t{output_lines[i]}")
+        print(f"actual:\t\t{actual_lines[i]}")
+        print("================")
         break
-
-    if isinstance(item, list):
-        # input with output
-        actual = actual_lines[cursor]
-
-        for actual_line in actual.splitlines(keepends=False):
-            expected = expected_lines[cursor]
-
-            if expected != actual_line:
-                ok = False
-                print("line mismatch.")
-                print(f">>> expected:\t{expected}")
-                print(f">>> actual:\t{actual_line}")
-                break
-            cursor += 1
-
-        if not ok:
-            break
-
-    tc_cursor += 1
+    i += 1
 
 if not ok:
-    print("test failed")
-    print("The last command of the following lines caused the error. Please copy to debug your code.")
-    print()
-    input_lines = input_str.splitlines(keepends=False)
-    for l in input_lines[:tc_cursor+1]:
-        print(l)
+    print('The last command of the following lines caused the error.')
+    print('Please copy these lines for debugging:')
+    x = belong_to[i]
+    i = 0
+    while i <= x:
+        print(input_lines[i])
+        i += 1
     exit(2)
 
-if len(actual_lines) != len(expected_lines):
+
+if len(actual_lines) != len(output_lines):
     print("test failed")
     print("last command output line count mismatch")
-    print("========")
-    print("expected:")
-    for l in expected_lines[cursor:]:
-        print(l)
 
-    print("========")
-    print("The last command(which has output) of the following lines caused the error. Please copy to debug your code.")
-    input_lines = input_str.splitlines(keepends=False)
-    for l in input_lines:
-        print(l)
+    if len(actual_lines) > len(output_lines):
+        print("actual lines are more than expected. extra lines:")
+        print("================")
+        for l in actual_lines[smaller_len:]:
+            print(l)
+        print("================")
+    else:
+        print("actual lines are less than expected. expect more lines:")
+        print("================")
+        for l in output_lines[smaller_len:]:
+            print(l)
+        print("================")
     exit(3)
 
-print('test passed!')
+print("test passed")
 exit(0)
